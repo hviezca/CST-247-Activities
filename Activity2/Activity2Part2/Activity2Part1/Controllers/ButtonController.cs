bad78158df6d30989978ee7dda2eb878efec7209
hviezca <hviezca@my.gcu.edu>
2021-05-02 19:58:46 -0600

    Activity 2 Part 2

---
+++ Activity2/Activity2Part2/Activity2Part1/Controllers/ButtonController.cs
@@ -0,0 +1,58 @@
+﻿using Activity2Part1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity2Part1.Controllers
+{
+    public class ButtonController : Controller
+    {
+        private static List<ButtonModel> buttons; 
+
+        // GET: Button
+        public ActionResult Index()
+        {
+            buttons = new List<ButtonModel>();
+            buttons.Add(new ButtonModel(true));
+            buttons.Add(new ButtonModel(false));
+
+            return View("Button", buttons);
+        }
+
+        public ActionResult OnButtonClick(string mine)
+        {
+            if (mine == "1")
+            {
+                if (buttons[0].State)
+                {
+                    buttons[0].State = false;
+                    return View("Button", buttons);
+                }
+                else
+                {
+                    buttons[0].State = true;
+                    return View("Button", buttons);
+                }
+            }
+            else if (mine == "2")
+            {
+                if (buttons[1].State)
+                {
+                    buttons[1].State = false;
+                    return View("Button", buttons);
+                }
+                else
+                {
+                    buttons[1].State = true;
+                    return View("Button", buttons);
+                }
+            }
+            else
+            {
+                return View("Button", buttons);
+            }
+        }
+    }
+}
---
+++ Activity2/Activity2Part2/Activity2Part1/Controllers/TestController.cs
@@ -0,0 +1,38 @@
+﻿using Activity2Part1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity2Part1.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            List<UserModel> model = new List<UserModel>();
+
+            model.Add(new UserModel()
+            {
+                Name = "Hiram",
+                EmailAddress = "Hiram.test@test.com",
+                PhoneNumber = "555-555-5555"
+            });
+            model.Add(new UserModel()
+            {
+                Name = "Sherlock",
+                EmailAddress = "Sherlock.test@test.com",
+                PhoneNumber = "555-541-5555"
+            });
+            model.Add(new UserModel()
+            {
+                Name = "Watson",
+                EmailAddress = "Watson.test@test.com",
+                PhoneNumber = "555-541-5574"
+            });
+            return View("Test", model);
+        }
+    }
+}

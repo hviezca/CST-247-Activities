7409358fcd4c1b616098cace098af07657ad91ce
hviezca <hviezca@my.gcu.edu>
2021-05-02 20:22:50 -0600

    Activity 2 Part 3

---
+++ Activity2/Activity2Part3/Activity1Part3/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Activity1Part3.Models;
+using Activity1Part3.Services.Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Activity1Part3.Controllers
+{
+    public class LoginController : Controller
+    {
+        // GET: Login
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        public ActionResult Login(UserModel model)
+        {
+            // Validate the Form POST
+            if (!ModelState.IsValid)
+                return View("Login");
+
+            SecurityService securityService = new SecurityService();
+            var result = securityService.Authenticate(model);
+
+            if (result)
+            {
+                return View("LoginPassed", model);
+            }
+            else
+            {
+                return View("LoginFailed");
+            }
+        }
+    }
+}
